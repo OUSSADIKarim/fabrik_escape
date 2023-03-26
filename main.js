a6ca9ac5d4372ef3f2a606a4f0ffbe6fe7904e04
OUSSADI Karim <oussadikarim00@gmial.com>
2023-03-26 21:27:01 +0100

    home + destination added

---
+++ main.js
@@ -0,0 +1,91 @@
+// nav logic
+const menuBtn = document.querySelector(".menu");
+const menuMobile = document.querySelector(".menu-mobile");
+const main = document.querySelector("main");
+
+menuBtn.addEventListener("click", () => {
+  menuBtn.classList.toggle("menu-clicked");
+  if (menuBtn.classList.contains("menu-clicked")) {
+    menuMobile.classList.add("menu-mobile-show");
+    main.classList.add("mainBefore");
+  } else {
+    menuMobile.classList.remove("menu-mobile-show");
+    main.classList.remove("mainBefore");
+  }
+});
+
+if (window.location.pathname === "/") {
+  // categories cards logic
+  const cardsContainer = document.querySelector(".cards-container");
+  let cardsData = [];
+  const cardsDataFetcher = fetch(
+    "./public/assets/simulation-data/destinations.json"
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      cardsData = data;
+      if (cardsData !== []) {
+        cardsData.forEach((cardData) => {
+          const card = `
+              <div class="card">
+                <img class="card-img" src="${cardData.img}" alt="">
+                <img class="card-icon" src="${cardData.icon}" alt="">
+                <p class="card-title">${cardData.title}</p>
+              </div>
+        `;
+
+          cardsContainer.innerHTML += card;
+        });
+      }
+    })
+    .catch((err) => console.error(err));
+
+  // guides cards logic
+  const guidesContainer = document.querySelector(".guide-container");
+  let guidesData = [];
+  const guidesDataFetcher = fetch("./public/assets/simulation-data/guides.json")
+    .then((response) => response.json())
+    .then((data) => {
+      guidesData = data;
+      if (guidesData !== []) {
+        guidesData.forEach((guideData) => {
+          const guide = `
+              <div class="guide card">
+                <img class="guide-img card-img" src="${guideData.img}" alt="">
+                <p class="guide-fullName card-title">${guideData.fullName}</p>
+              </div>`;
+
+          guidesContainer.innerHTML += guide;
+        });
+      }
+    })
+    .catch((err) => console.error(err));
+}
+
+if (window.location.pathname === "/public/destinations.html") {
+  // destinations logic
+  const destinationsContainer = document.querySelector(
+    ".destinations-container"
+  );
+  let destinationsData = [];
+
+  const destinationsDataFetcher = fetch(
+    "./assets/simulation-data/destinations.json"
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      destinationsData = data;
+      if (destinationsData !== []) {
+        destinationsData.forEach((destinationData) => {
+          const destination = `
+            <div class="guide card">
+              <img class="guide-img card-img" src="${destinationData.img}" alt="">
+              <p class="guide-fullName card-title">${destinationData.title}</p>
+            </div>`;
+
+          destinationsContainer.innerHTML += destination;
+        });
+      }
+    })
+    .catch((err) => console.error(err));
+}
